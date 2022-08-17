b7166112d16302a180dab1ec38bd7cf9bd133b71
Oscartalamantes <97889238+Oscartalamantes@users.noreply.github.com>
2022-08-17 06:52:59 -0600

    Update getMail.php
    fixed $messages again

--- assets/php/getMail.php
+++ assets/php/getMail.php
@@ -1,3 +1,4 @@
+
 <?php include 'assets/php/Upload_Files.php'
 ?>
 
@@ -35,18 +36,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	if ($subject == '') { $subject = "Contact Form Submission"; }
 
    // Set Message
-   $message .= "Email from: " . $fullname . "<br />";
-   $message .= "Email address: " . $email . "<br />";
-   $message .= "Phone: " . $phone . "<br />";
-   $message .= "Additional: " . $Additional . "<br />";
-   $message .= "goal: " . $goal . "<br />";
-   $message .= "Work: " . $Work . "<br />";
-   $message .= "Service: " . $Service . "<br />";
-   $message .= "Education: " . $Education . "<br />";
-   $message .= nl2br($contact_message);
-   $message .= "<br /> ----- <br /> This email was sent from your site " . $_SERVER['SERVER_NAME'] . "<br />" ;
-   
-
+     $message .= "Email from: " . $fullname . "<br />";
+     $message .= "Email address: " . $email . "<br />";
+     $message .= "Phone: " . $phone . "<br />";
+     $message .= "Education: " . $Education . "<br />";
+     $message .= "Service: " . $Service . "<br />";
+     $message .= "Work: " . $Work . "<br />";
+     $message .= "Additional: " . $Additional . "<br />";
+     $message .= "fileToUpload: " . $fileToUpload . "<br />";
+     
+     
    // Set From: header
    $from =  $fullname . " <" . $email . ">";
 
